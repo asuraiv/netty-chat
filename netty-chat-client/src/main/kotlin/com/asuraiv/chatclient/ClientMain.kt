15ee6c8854d483ed2671a6873fb38a18eb5d01b3
asuraiv <asuraiv7@gmail.com>
2020-05-06 12:58:38 +0900

    Kotlin 환경 세팅

---
+++ netty-chat-client/src/main/kotlin/com/asuraiv/chatclient/ClientMain.kt
@@ -0,0 +1,12 @@
+package com.asuraiv.chatclient
+
+class ClientMain {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Chat Client.")
+        }
+    }
+}
